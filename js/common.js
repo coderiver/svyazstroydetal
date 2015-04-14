949f6e99729fd7226da5f89d6d384c67bd659b2c
Eugene Godun <eugene.godun@gmail.com>
2015-04-14 12:53:09 +0300

    index

--- js/common.js
+++ js/common.js
@@ -4,15 +4,30 @@ head.ready(function() {
 	// 	$(".js-popup").hide();
 	// });
 
-	// function scrollFixedElements() {
-	//     var scroll_left = $(this).scrollLeft();
-	//     $(".fixed-element").css({
-	//         left: -scroll_left
-	//     });
-	// }
-	// scrollFixedElements();
-	// $(window).scroll(function(){
-	//     scrollFixedElements()
-	// });
+	 function tabsLoad() {
+	     var hash = window.location.hash;
+	     if (hash) {
+	         $('[href="'+hash+'"]').parents(".js-tabs-group").find(".js-tabs-content").hide();
+	         $('[data-id="'+hash+'"]').show();
+	         $('[href="'+hash+'"]').parents(".js-tabs").find("li").removeClass("is-active");
+	         $('[href="'+hash+'"]').parent().addClass("is-active");
+	     }
+	     else {
+	         $('.js-tabs li:first').addClass("is-active");
+	         $('.js-tabs').next().show();
+	     }
+	     
+	 }
+	tabsLoad();
+	// alert(hash);
+	 $('.js-tabs a').on("click",function (e) {
+	     var content = $(this).attr("href");
+	     $(this).parents(".js-tabs").find("li").removeClass("is-active");
+	     $(this).parent().addClass("is-active");
+	     $(this).parents(".js-tabs-group").find(".js-tabs-content").hide();
+	     $('[data-id="'+content+'"]').show();
+	     window.location.hash = this.hash;
+	     return false;
+	 });
 
 });
