ec0e5fb380c39f726a6157a24e38eebc8e1af23b
Eugene Godun <eugene.godun@gmail.com>
2015-04-17 16:25:35 +0300

    basket

--- js/common.js
+++ js/common.js
@@ -130,5 +130,51 @@ head.ready(function() {
 		
 	validate();
 
+	function number() { 
+        var number = $(".js-number");
+        number.each(function(){
+            var max_number = +($(this).attr("data-max-number"));
+            var input = $(this).find("input");
+            var plus = $(this).find(".js-plus-number");
+            var minus = $(this).find(".js-minus-number");
+            plus.on("click", function(){
+                var val = +(input.val());
+                if (val >= max_number) {
+                    return false
+                }
+                else {
+                    val += 1;
+                    input.val(val);
+                }
+            });
+            minus.on("click", function(){
+                var val = +(input.val());
+                if (val > 1) {
+                    val -= 1;
+                    input.val(val);
+                }
+                else {
+                    return false;
+                }
+            });
+            input.on("change", function(){
+                var val = +$(this).val();
+                if (val > max_number) {
+                    val = max_number;
+                    $(this).val(val);
+                }
+                else {
+                    return false;
+                }
+            });
+        });
+    }
+    number();
+
+	$(".js-del-parent").on("click", function(){
+        $(this).closest(".js-parent").remove();
+		return false;
+	});
+
 
 });
